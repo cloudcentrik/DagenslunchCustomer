c5acff663c66d632ca6fa94432401e14d58b4c0e
Jobaer <java2.cloudcentrik@gmail.com>
2017-01-23 19:52:40 +0200

    JobaerTestPush

---
+++ app/src/main/java/net/cloudcentrik/dagenslunchcustomer/GeolocationCalculator.java
@@ -0,0 +1,16 @@
+package net.cloudcentrik.dagenslunchcustomer;
+
+import android.util.Log;
+
+/**
+ * Created by User on 23-Jan-17.
+ */
+
+public class GeolocationCalculator {
+    void calculation(){
+        int n = 2+2;
+      //  System.out.println(n);
+        Log.d("Hello", "calculation: ");
+    }
+
+}
