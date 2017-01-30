ef60229704824e86f9b406b9c691dfa8eebab798
Java1 Developer <java1.cloudcentrik@gmail.com>
2017-01-30 17:46:34 +0100

    Json client request Not Done!!

--- app/src/main/java/net/cloudcentrik/dagenslunchcustomer/MainActivity.java
+++ app/src/main/java/net/cloudcentrik/dagenslunchcustomer/MainActivity.java
@@ -1,9 +1,25 @@
 package net.cloudcentrik.dagenslunchcustomer;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/*
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,4 +29,103 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+}
+*/
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView output;
+    String loginURL = "http://dagens-lunch-v1.herokuapp.com/people"; // your URL
+    String data = "";
+
+    RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            Log.d("STATE", savedInstanceState.toString());
+        }
+
+        requestQueue = Volley.newRequestQueue(this);
+        output = (TextView) findViewById(R.id.jsonData);
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, loginURL, (String)null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        output.setText("Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        Log.e("Volley","Error");
+
+                    }
+                });
+        // Access the RequestQueue through your singleton class.
+        //MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+
+/*        JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, loginURL, (String)null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try{
+
+                            JSONArray ja = response.getJSONArray("posts");
+
+                            for(int i=0; i < ja.length(); i++){
+
+                                JSONObject jsonObject = ja.getJSONObject(i);
+
+                                // int id = Integer.parseInt(jsonObject.optString("id").toString());
+                                String title = jsonObject.getString("title");
+                                String url = jsonObject.getString("URL");
+
+                                data += "Blog Number "+(i+1)+" \n Blog Name= "+title  +" \n URL= "+ url +" \n\n\n\n ";
+                            }
+
+                            output.setText(data);
+                        }catch(JSONException e){e.printStackTrace();}
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley","Error");
+
+                    }
+                }
+        );
+        requestQueue.add(jor);*/
+    }
+
+ /*   @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+
 }
