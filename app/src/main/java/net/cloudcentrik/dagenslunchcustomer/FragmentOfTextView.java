589614bc50f195c7989d65707d9e042bfb491e91
java4-cloudcentrik <java4.cloudcentrik@gmail.com>
2017-02-13 12:01:35 -0800

    jahir task2

---
+++ app/src/main/java/net/cloudcentrik/dagenslunchcustomer/FragmentOfTextView.java
@@ -0,0 +1,31 @@
+package net.cloudcentrik.dagenslunchcustomer;
+
+/**
+ * Created by Johir on 2/7/2017.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentOfTextView extends Fragment {
+
+    public FragmentOfTextView() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_fragment_textview, container, false);
+    }
+
+}
