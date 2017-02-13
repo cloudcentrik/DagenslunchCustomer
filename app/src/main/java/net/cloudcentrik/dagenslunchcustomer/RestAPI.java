c56164e0984aec99eb9e873ebb7c4d06047b9506
ismail fakir <info.cloudcentrik@gmail.com>
2017-02-13 20:14:57 +0100

    initial Restaurant list added again

--- app/src/main/java/net/cloudcentrik/dagenslunchcustomer/RestAPI.java
+++ app/src/main/java/net/cloudcentrik/dagenslunchcustomer/RestAPI.java
@@ -11,9 +11,13 @@ public class RestAPI {
     public static ArrayList<Restaurant> getListOfRestaurant(){
         ArrayList<Restaurant> restaurants=new ArrayList<Restaurant>();
 
-        Restaurant restaurant1=new Restaurant(1,"pizza hurt","pizza",58.7089,11.9746);
+        Restaurant restaurant1=new Restaurant(1,"pizza hurt","pizza",58.7189,11.9746);
+        Restaurant restaurant2=new Restaurant(2,"pizza hurt","pizza",58.7289,11.9756);
+        Restaurant restaurant3=new Restaurant(3,"pizza hurt","pizza",58.7389,11.9766);
 
         restaurants.add(restaurant1);
+        restaurants.add(restaurant2);
+        restaurants.add(restaurant3);
 
         return restaurants;
     }
