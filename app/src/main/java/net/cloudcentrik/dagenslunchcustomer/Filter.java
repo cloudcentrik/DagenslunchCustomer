274e6f216fbfac676df7a7055c9b76799a63bcd2
java5-cloudcentrik <java5.cloudcentrik@gmail.com>
2017-02-23 15:57:29 +0100

    hard coded restaurants

--- app/src/main/java/net/cloudcentrik/dagenslunchcustomer/Filter.java
+++ app/src/main/java/net/cloudcentrik/dagenslunchcustomer/Filter.java
@@ -1,5 +1,7 @@
 package net.cloudcentrik.dagenslunchcustomer;
 
+import android.util.Log;
+
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
@@ -9,7 +11,34 @@ import java.util.ArrayList;
  */
 
 public class Filter {
-    public ArrayList<Restaurant> filterRestaurants(ArrayList<Restaurant> restaurants, int howFar){
+
+
+
+    static ArrayList<Restaurant> getRestaurants(){
+        Restaurant res1 = new Restaurant(1, "Bee bar Göteborg", "X", 57.703540, 11.967894);
+        Restaurant res2 = new Restaurant(2, "Bee bar Malmö", "X", 58.508393, 13.135094);
+        Restaurant res3 = new Restaurant(3, "Hard rock cafe", "Y", 57.701381, 11.974537);
+        Restaurant res4 = new Restaurant(4, "Göteborg wok", "Y", 57.709316, 11.966394);
+        Restaurant res5 = new Restaurant(5, "Göteborg wok", "Z", 57.722906, 11.954941);
+        Restaurant res6 = new Restaurant(6, "Restaurang Saray", "Z", 57.741848, 12.008720);
+        Restaurant res7 = new Restaurant(7, "Fiskekrogen", "A", 57.705526, 11.961373);
+        Restaurant res8 = new Restaurant(8, "Hello Monkey", "A", 57.693591, 11.953009);
+        ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
+        restaurants.add(res1);
+        restaurants.add(res2);
+        restaurants.add(res3);
+        restaurants.add(res4);
+        restaurants.add(res5);
+        restaurants.add(res6);
+        restaurants.add(res7);
+        restaurants.add(res8);
+        return restaurants;
+    }
+
+
+
+
+    public static ArrayList<Restaurant> filterRestaurants(ArrayList<Restaurant> restaurants, int howFar){
         ArrayList<Restaurant>  filteredRestaurants = new ArrayList<Restaurant>();
 
         LatLng currentLocation = getCurrentLocation();
@@ -21,6 +50,7 @@ public class Filter {
                 filteredRestaurants.add(restaurants.get(i));
             }
         }
+        //Log.d("hi", "hello");
         return filteredRestaurants;
     }
 
@@ -28,14 +58,22 @@ public class Filter {
 
 
 
-    public LatLng getCurrentLocation() {
+    public static LatLng getCurrentLocation() {
         LatLng currentLocation = new LatLng(1.22, 2.33);
         return currentLocation;
     }
 
-    public double getDistance(LatLng location1, LatLng location2){
+    public static double getDistance(LatLng location1, LatLng location2){
         double distance = 1.00;
         return distance;
+
+    }
+
+    public static void main(String[] args){
+        ArrayList<Restaurant> a = new ArrayList<Restaurant>();
+        Restaurant r = new Restaurant(1, "hi","hello", 4.336, 6.778);
+        a.add(r);
+        filterRestaurants(a, 10);
 
     }
 }
