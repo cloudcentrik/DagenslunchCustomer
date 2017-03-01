8043a3880c0d7f7aa7b8d199079733535e43a2c8
java4-cloudcentrik <java4.cloudcentrik@gmail.com>
2017-03-01 07:18:38 -0800

    after fixing problem of map

--- app/src/main/java/net/cloudcentrik/dagenslunchcustomer/MapActivity.java
+++ app/src/main/java/net/cloudcentrik/dagenslunchcustomer/MapActivity.java
@@ -44,9 +44,9 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in usa and move the camera
-        LatLng sydney = new LatLng(-34, 243);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in California"));
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
