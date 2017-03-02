10eb647f55f75f11ad88838aef22048186a5bfe7
Java1 Developer <java1.cloudcentrik@gmail.com>
2017-03-02 20:40:29 +0100

    listview updated!!

--- app/src/main/java/net/cloudcentrik/dagenslunchcustomer/FragmentOfTextView.java
+++ app/src/main/java/net/cloudcentrik/dagenslunchcustomer/FragmentOfTextView.java
@@ -15,6 +15,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
 import java.util.ArrayList;
 //import com.android.volley.RequestQueue;
 
@@ -42,82 +45,28 @@ public class FragmentOfTextView extends Fragment {
 
         View rootView = inflater.inflate(R.layout.activity_fragment_textview, container, false);
 
-        // Defined Array values to show in ListView
-        /*restauratNames.add("Restaurant ABC");
-        restauratNames.add("Restaurant XYZ");
-        restauratNames.add("ABC");
-        restauratNames.add("PQR");
-        restauratNames.add("PIZZA");
-        restauratNames.add("PIZZA Campus");
-        restauratNames.add("GBG BEST PIZZA");*/
-
-
-        /*
-
-        requestQueue = Volley.newRequestQueue(getActivity());
-
-        JsonArrayRequest jsArrRequest = new JsonArrayRequest(com.android.volley.Request.Method.GET, uRL_json_array, (String) null, new com.android.volley.Response.Listener<JSONArray>()
-        {
-
-            @Override
-            public void onResponse(JSONArray response) {
-                //output_Json_Arr.setText("Response: " + response.toString());
-                //String str_json_array = response.toString();
-                //output_Json_Arr.setText(str_json_array);
-
-                String strJA = response.toString();
-                try {
-                    JSONArray jsonarray = new JSONArray(strJA);
-                    Gson gson2 = new Gson(); // Or use new GsonBuilder().create();
-                    PeopleJsonData targetJArr = new PeopleJsonData();
-
-                    for (int i = 0; i < jsonarray.length(); i++) {
-                        JSONObject jsonobject = jsonarray.getJSONObject(i);
-                        String str_JObj_of_Arr = jsonobject.toString();
-                        targetJArr = gson2.fromJson(str_JObj_of_Arr, PeopleJsonData.class); // deserializes json into target
-                        restauratNames.add(targetJArr.fullName);
-                        //output_Json_Arr.setText(targetJArr.fullName +" "+ targetJArr.jobTitle);
-                        System.out.println(targetJArr.fullName);
-
-                    }
-                } catch (Exception e)
-                {
-                    e.getStackTrace();
-                }
-
-            }
-        }, new com.android.volley.Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                // TODO Auto-generated method stub
-                Toast.makeText(getActivity(), "Error in JSON", Toast.LENGTH_SHORT).show();
-                Log.e("Volley","Error");
-
-            }
-        });
-        requestQueue.add(jsArrRequest);
-
-*/
-
-
 
         ArrayList<Restaurant> restaurantsObj = new ArrayList<Restaurant>();
         restaurantsObj = Filter.getRestaurants();
 
+        double [] distanceOfRestaurants = new double[100];
+
+        LatLng currentLocation = new LatLng(0,0);
+        currentLocation = Filter.getCurrentLocation();
+
+        LatLng restaurantLocation = new LatLng(0,0);
 
         for (int i = 0; i < restaurantsObj.size(); i++) {
-            restauratNames.add(restaurantsObj.get(i).getName() + "         " + "Type: " + restaurantsObj.get(i).getCategory() );
+            distanceOfRestaurants[i] = Filter.getDistance(currentLocation, restaurantLocation);
+            restauratNames.add(restaurantsObj.get(i).getName() + "     " + distanceOfRestaurants[i] + " meters");
 
         }
-
 
         // Get ListView object from xml
         listView = (ListView) rootView.findViewById(R.id.list2);
 
         adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, restauratNames);
-
 
         // Assign adapter to ListView
         listView.setAdapter(adapter);
@@ -150,56 +99,3 @@ public class FragmentOfTextView extends Fragment {
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-package net.cloudcentrik.dagenslunchcustomer;
-
-*/
-/**
- * Created by Johir on 2/7/2017.
- *//*
-
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-
-public class FragmentOfTextView extends Fragment {
-
-    public FragmentOfTextView() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_fragment_textview, container, false);
-    }
-
-}
-*/
